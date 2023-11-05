31b6a312924fdd6eb1ac7cf816f50ebfa9d99634
Marcos Turo <102767266+MTuro@users.noreply.github.com>
2023-11-05 17:13:32 -0300

    Create mancala.py

---
+++ mancala.py
@@ -0,0 +1,22 @@
+import pygame
+
+def realiza_jogada():{
+    
+}
+
+
+def verifica_fim():{
+    
+}
+
+def valida_jogada():{
+    
+}
+
+def pega_todas_pecas():{
+    
+}
+
+def captura_peca():{
+    
+}
