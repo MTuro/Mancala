84bb087d480172eed599b7858bb2827d1dd789d6
Nathan Muniz <102868758+NathanMuniz18@users.noreply.github.com>
2023-11-10 09:24:52 -0300

    Update partida.py
    fiz esse codigo de verifica fim e botei ele todo, deixa esse arquivo de rascunho ate todas as funcs estarem prontas e ai a gente bota o arquivo final no lugar quando tudo estiver pronto

--- partida.py
+++ partida.py
@@ -5,9 +5,27 @@ def realiza_jogada():{
 }
 
 
-def verifica_fim():{
-    
-}
+########################## verifica fim ##################################
+jogador1 = [4,4,4,4,4,4,0]
+jogador2 = [4,4,4,4,4,4,0]
+
+def somar_elementos(lista):
+  soma = 0
+  for numero in lista:
+    soma += numero
+  return soma
+
+
+soma_jogador1 = somar_elementos(jogador1)
+soma_jogador2 = somar_elementos(jogador2)
+
+def verifica_fim():
+    if soma_jogador1 == 0 or soma_jogador2 == 0:
+        return pega_todas_pecas()
+    return
+##############################################################################
+
+
 
 def valida_jogada():{
     
